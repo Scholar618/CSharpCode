8b84eba3cd163ed282bbd8cf0e8590f44076af3e
Scholar618 <641079547@qq.com>
2025-05-11 23:10:10 +0800

    补充实体层搭建

---
+++ 学员管理系统/Models/ScoreList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    /// <summary>
+    /// 成绩列表实体类
+    /// </summary>
+    public class ScoreList
+    {
+    }
+}
---
+++ 学员管理系统/Models/SysAdmin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    /// <summary>
+    /// 管理员类
+    /// </summary>
+    public class SysAdmin
+    {
+        public int LoginId { get; set; }
+        public string AdminName { get; set; }
+        public string LoginPwd { get; set; }
+    }
+}
