d181e7a559c76b8a52c50a6ec04494c8086921fa
Scholar618 <641079547@qq.com>
2025-05-26 14:30:23 +0800

    委托的使用

---
+++ Demo/DelegateDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    // 委托是一种类，与Program同级别
+    public delegate int Calc(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Action action = new Action(cal.Report);
+            action.Invoke();
+            action();
+
+            Func<int, int, int> func1 = new Func<int, int, int>(cal.Add);
+            Func<int, int, int> func2 = new Func<int, int, int>(cal.Sub);
+            func1.Invoke(5, 4);
+            func2.Invoke(5, 4);
+            func1(5, 4);
+            func2(5, 4);
+
+            // 自定义委托
+            Calc calc1 = new Calc(cal.Add);
+            Calc calc2 = new Calc(cal.Sub);
+            calc1.Invoke(5, 4);
+            calc2.Invoke(5, 4);
+        }
+    }
+
+    class Calculator
+    {
+        public void Report()
+        {
+            Console.WriteLine("I have 3 method!");
+        }
+
+        public int Add(int a, int b)
+        {
+            Console.WriteLine("a + b = {0}", a+b);
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            Console.WriteLine("a - b = {0}", a - b);
+            return a - b;
+        }
+
+    }
+}
---
+++ Demo/TemplateDelegata/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateDelegata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductFactory productFac = new ProductFactory();
+            WrapFactory wrapFactory = new WrapFactory();
+
+            Func<Product> func1 = new Func<Product>(productFac.MakePizza);
+            Func<Product> func2 = new Func<Product>(productFac.MakeToyCar);
+
+            Logger logger = new Logger();
+            Action<Product> log = new Action<Product>(logger.Log);
+
+            Box box1 = wrapFactory.WrapProduct(func1, log);
+            Box box2 = wrapFactory.WrapProduct(func2, log);
+
+            Console.WriteLine(box1.Product.Name);
+            Console.WriteLine(box2.Product.Name);
+        }
+    }
+
+    class Logger
+    {
+        public void Log(Product product)
+        {
+            Console.WriteLine("Product '{0}' created at {1}.Price is {2}!", product.Name, DateTime.UtcNow, product.Price);
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+    class Box
+    {
+        public Product Product { get; set; }
+    }
+
+    class WrapFactory
+    {
+        // 模板方法实现委托
+        public Box WrapProduct(Func<Product> getProduct, Action<Product> getLog)
+        {
+            Box box = new Box();
+            Product product = getProduct.Invoke();
+            if(product.Price > 20)
+            {
+                getLog(product);
+            }
+            box.Product = product;
+            return box;
+        }
+    }
+    
+    //作用是委托一旦形成，上面几个类后续都可以不用变化，想要增加方法只需在ProductFactory里增加即可
+    //提高代码复用性
+
+    class ProductFactory
+    { 
+        public Product MakePizza()
+        {
+            Product product = new Product();
+            product.Name = "Pizza";
+            product.Price = 12;
+            return product;
+        }
+
+        public Product MakeToyCar()
+        {
+            Product product = new Product();
+            product.Name = "ToyCar";
+            product.Price = 120;
+            return product;
+        }
+    }
+
+}
